1182335aa898f2f495996002428fe2a141421b91
ylizzr <ylizarazor@ucentral.edu.co>
2025-04-06 12:53:56 -0500

    Estructura

---
+++ src/test/java/com/energiainteligente/authservice/AuthserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energiainteligente.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
