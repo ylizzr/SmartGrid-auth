05a7f8bcc5e27c9a0cb53482122f6b63af340134
ylizzr <ylizarazor@ucentral.edu.co>
2025-05-04 09:33:56 -0500

    ajuste validacion empleado

---
+++ src/main/java/com/energiainteligente/authservice/controladores/EmpleadoControlador.java
@@ -0,0 +1,37 @@
+package com.energiainteligente.authservice.controladores;
+
+import com.energiainteligente.authservice.servicios.EmpleadoServicio;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+
+@Controller
+public class EmpleadoControlador {
+
+    private final EmpleadoServicio empleadoServicio;
+
+    public EmpleadoControlador(EmpleadoServicio empleadoServicio) {
+        this.empleadoServicio = empleadoServicio;
+    }
+
+    @GetMapping("/validar-empleado")
+    public String mostrarFormularioValidacion() {
+        return "validar-empleado";
+    }
+
+    @PostMapping("/empleado/validar")
+    @ResponseBody
+    public ResponseEntity<?> validarEmpleado(@RequestParam String cedula) {
+        boolean existe = empleadoServicio.validarEmpleadoPorCedula(cedula);
+        if (existe) {
+            return ResponseEntity.ok().body(Collections.singletonMap("mensaje", "Conectado correctamente"));
+        } else {
+            return ResponseEntity
+                    .badRequest()
+                    .body(Collections.singletonMap("mensaje", "Cédula no encontrada"));
+        }
+    }
+
+}
--- src/main/java/com/energiainteligente/authservice/servicios/EmpleadoServicio.java
+++ src/main/java/com/energiainteligente/authservice/servicios/EmpleadoServicio.java
@@ -4,14 +4,13 @@ import com.energiainteligente.authservice.persistencia.repositorio.EmpleadoRepos
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class EmpleadoServicio {
+
     @Autowired
     private EmpleadoRepositorio empleadoRepositorio;
 
-    public boolean existePorCedula(String cedula) {
+    public boolean validarEmpleadoPorCedula(String cedula) {
         return empleadoRepositorio.existsByCedula(cedula);
     }
 }
-
