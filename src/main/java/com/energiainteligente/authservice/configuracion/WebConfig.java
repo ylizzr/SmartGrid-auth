f53263e1710fddfb5a7ec359080b516674c66eb3
ylizzr <ylizarazor@ucentral.edu.co>
2025-05-26 23:04:46 -0500

    Ajuste direccion puerto front

--- src/main/java/com/energiainteligente/authservice/configuracion/WebConfig.java
+++ src/main/java/com/energiainteligente/authservice/configuracion/WebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig {
 
@@ -14,11 +13,17 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://35.208.6.42:8082")
+                        .allowedOrigins(
+                                "http://35.208.6.42:8082",
+                                "http://localhost:8082",
+                                "http://energiainteligente.ddns.net:8082"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowCredentials(true);
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization", "Set-Cookie")
+                        .allowCredentials(true)
+                        .maxAge(3600);
             }
-
         };
     }
 }
